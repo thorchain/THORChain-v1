3f69091b0d31ad8a3d5d2edcd695cafb0a329a90
Philip Stanislaus <stanislaus.philip@gmail.com>
2018-08-28 20:57:18 +0200

    Create first version of scripts

---
+++ cmd/thorchainspam/account/constants.go
@@ -0,0 +1,12 @@
+package account
+
+// nolint
+const (
+	SpamAccountPrefix   = "spam"
+	SpamAccountPassword = "1234567890"
+	FlagFrom            = "from"
+	FlagK               = "k"
+	FlagAmount          = "amount"
+	FlagChainID         = "chain-id"
+	FlagNode            = "node"
+)
