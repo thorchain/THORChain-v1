2c51758b6d072ea45cb448237b581730153604f9
Aidan Musnitzky <musnit@gmail.com>
2018-08-30 14:03:49 +0100

    Add temp fix for api automatically getting nonces

--- x/clp/client/rest/tx.go
+++ x/clp/client/rest/tx.go
@@ -1,6 +1,7 @@
 package clp
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
 
@@ -12,6 +13,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/context"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/wire"
+	authcmd "github.com/cosmos/cosmos-sdk/x/auth/client/cli"
 	"github.com/cosmos/cosmos-sdk/x/bank"
 )
 
@@ -53,8 +55,37 @@ func buildTradeBaseMsg(from sdk.AccAddress, m sendBody) sdk.Msg {
 	return clpTypes.NewMsgTradeBase(from, m.Ticker, m.RuneAmount)
 }
 
+//Ensure Account Number is added to context for transaction
+func EnsureAccountNumber(ctx context.CoreContext, m sendBody, from sdk.AccAddress) (context.CoreContext, error) {
+	if m.AccountNumber != 0 {
+		return ctx, nil
+	}
+	accnum, err := ctx.GetAccountNumber(from)
+	if err != nil {
+		return ctx, err
+	}
+	fmt.Printf("Defaulting to account number: %d\n", accnum)
+	ctx = ctx.WithAccountNumber(accnum)
+	return ctx, nil
+}
+
+//Ensure Sequence is added to context for transaction
+func EnsureSequence(ctx context.CoreContext, m sendBody, from sdk.AccAddress) (context.CoreContext, error) {
+	if m.Sequence != 0 {
+		return ctx, nil
+	}
+	seq, err := ctx.NextSequence(from)
+	if err != nil {
+		return ctx, err
+	}
+	fmt.Printf("Defaulting to next sequence number: %d\n", seq)
+	ctx = ctx.WithSequence(seq)
+	return ctx, nil
+}
+
 // SendRequestHandlerFn - http request handler to send coins to a address
 func SendRequestHandlerFn(cdc *wire.Codec, kb keys.Keybase, ctx context.CoreContext, msgBuilder func(sdk.AccAddress, sendBody) sdk.Msg) http.HandlerFunc {
+	ctx = ctx.WithDecoder(authcmd.GetAccountDecoder(cdc))
 	return func(w http.ResponseWriter, r *http.Request) {
 		var m sendBody
 		body, err := ioutil.ReadAll(r.Body)
@@ -90,12 +121,25 @@ func SendRequestHandlerFn(cdc *wire.Codec, kb keys.Keybase, ctx context.CoreCont
 
 		// add gas to context
 		ctx = ctx.WithGas(m.Gas)
+
 		// add chain-id to context
 		ctx = ctx.WithChainID(m.ChainID)
 
-		// sign
-		ctx = ctx.WithAccountNumber(m.AccountNumber)
-		ctx = ctx.WithSequence(m.Sequence)
+		//add account number and sequence
+		ctx, err = EnsureAccountNumber(ctx, m, from)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		ctx, err = EnsureSequence(ctx, m, from)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		//sign
 		txBytes, err := ctx.SignAndBuild(m.LocalAccountName, m.Password, []sdk.Msg{msg}, cdc)
 		if err != nil {
 			w.WriteHeader(http.StatusUnauthorized)
