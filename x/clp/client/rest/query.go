edaf5aab06f8d46424fe9518bfbd6243a11801d7
Aidan Musnitzky <musnit@gmail.com>
2018-09-07 13:35:21 +0200

    Add endpoint/cli for all clps

--- x/clp/client/rest/query.go
+++ x/clp/client/rest/query.go
@@ -22,6 +22,11 @@ func registerQueryRoutes(ctx context.CoreContext, r *mux.Router, cdc *wire.Codec
 		"/clp/{ticker}",
 		QueryAccountRequestHandlerFn(cdc, authcmd.GetAccountDecoder(cdc), ctx),
 	).Methods("GET")
+	r.HandleFunc(
+		"/clps",
+		QueryAllRequestHandlerFn(cdc, authcmd.GetAccountDecoder(cdc), ctx),
+	).Methods("GET")
+
 }
 
 // query accountREST Handler
@@ -61,6 +66,59 @@ func QueryAccountRequestHandlerFn(cdc *wire.Codec, decoder auth.AccountDecoder, 
 		}
 
 		w.Write(output)
+
+	}
+}
+
+// query all Handler
+func QueryAllRequestHandlerFn(cdc *wire.Codec, decoder auth.AccountDecoder, ctx context.CoreContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.NewCoreContextFromViper()
+
+		clpSubspace := []byte("clp:")
+
+		res, err := ctx.QuerySubspace(cdc, clpSubspace, "clp")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		if res == nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		var clps []clpTypes.CLP
+		for i := 0; i < len(res); i++ {
+			clp := new(clpTypes.CLP)
+			err2 := cdc.UnmarshalBinary(res[i].Value, &clp)
+			if err2 != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(fmt.Sprintf("couldn't parse query result. Result: %s. Error: %s", res, err2.Error())))
+				return
+			}
+			clps = append(clps, *clp)
+		}
+		fmt.Printf("CLP details \n\n")
+
+		var outputs [][]byte
+		for i := 0; i < len(clps); i++ {
+			output, err3 := cdc.MarshalJSON(clps[i])
+			if err3 != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(fmt.Sprintf("couldn't marshall query result. Error: %s", err3.Error())))
+				return
+			}
+			outputs = append(outputs, output)
+		}
+		w.Write([]byte("["))
+
+		for i := 0; i < len(outputs); i++ {
+			w.Write(outputs[i])
+			if i != len(outputs)-1 {
+				w.Write([]byte(","))
+			}
+		}
+		w.Write([]byte("]"))
 
 	}
 }
