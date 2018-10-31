773b151f4d2bde68608ddfed8c71bbe2bc02e1c2
Philip Stanislaus <stanislaus.philip@gmail.com>
2018-10-31 13:31:11 +0100

    Add spammer deployment config

--- networks/remote/terraform-spam/cluster/outputs.tf
+++ networks/remote/terraform-spam/cluster/outputs.tf
@@ -8,8 +8,11 @@ output "instances" {
   value = ["${aws_instance.cluster.*.id}"]
 }
 
+#output "instances_count" {
+#  value = "${length(aws_instance.node.*)}"
+#}
+
 // The list of cluster instance public IPs
 output "public_ips" {
   value = ["${aws_eip.eip.*.public_ip}"]
 }
-
