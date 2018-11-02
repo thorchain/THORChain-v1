a610cc61b9405fb53b36047b202c66f1a0f33fc6
Philip Stanislaus <stanislaus.philip@gmail.com>
2018-11-02 21:28:44 +0100

    Add creation of limit orders/wash trading to thorchain spammer

--- cmd/thorchainspam/txs/send.go
+++ cmd/thorchainspam/txs/send.go
@@ -23,11 +23,18 @@ import (
 	"github.com/thorchain/THORChain/cmd/thorchainspam/log"
 	"github.com/thorchain/THORChain/cmd/thorchainspam/stats"
 	clpTypes "github.com/thorchain/THORChain/x/clp/types"
+	"github.com/thorchain/THORChain/x/exchange"
 )
 
 var (
 	defaultBlockTime time.Duration = 1000
 )
+
+type spamCtx struct {
+	chainID         string
+	stats           *stats.Stats
+	limitOrderPairs [][]string
+}
 
 // Returns the command to send txs between accounts
 func GetTxsSend(cdc *wire.Codec) func(cmd *cobra.Command, args []string) error {
@@ -48,8 +55,15 @@ func GetTxsSend(cdc *wire.Codec) func(cmd *cobra.Command, args []string) error {
 
 		stats := stats.NewStats()
 
+		limitOrderPairs, err := parseLimitOrderPairs(viper.GetString(FlagLimitOrderPairs))
+		if err != nil {
+			return err
+		}
+
+		spamCtx := spamCtx{chainID, &stats, limitOrderPairs}
+
 		// create context and all spammer objects
-		spammers, err := createSpammers(cdc, spamPrefix, spamPassword, &stats, chainID)
+		spammers, err := createSpammers(&spamCtx, cdc, spamPrefix, spamPassword)
 		if err != nil {
 			return err
 		}
@@ -67,14 +81,14 @@ func GetTxsSend(cdc *wire.Codec) func(cmd *cobra.Command, args []string) error {
 		// rate limiter to allow x events per second
 		limiter := time.Tick(time.Duration(rateLimit) * time.Millisecond)
 
-		go doEvery(1*time.Second, stats.Print)
+		go doEvery(1*time.Second, spamCtx.stats.Print)
 
 		i := 0
 
 		for {
 			<-limiter
 			nextSpammer := spammers[(i+1)%len(spammers)]
-			go spammers[i].send(&nextSpammer, &stats)
+			go spammers[i].send(&spamCtx, &nextSpammer)
 			if i == len(spammers)-1 {
 				i = 0
 			} else {
@@ -84,14 +98,35 @@ func GetTxsSend(cdc *wire.Codec) func(cmd *cobra.Command, args []string) error {
 	}
 }
 
+func parseLimitOrderPairs(input string) (result [][]string, err error) {
+	if input == "" {
+		err = fmt.Errorf("--limit-order-pairs is required")
+		return
+	}
+
+	pairs := strings.Split(input, ",")
+
+	for _, pair := range pairs {
+		denoms := strings.Split(pair, ":")
+
+		if len(denoms) != 2 {
+			err = fmt.Errorf("--limit-order-pairs contained invalid pair '%v', needs to be of format 'TKNA:TKNB'", pair)
+			return
+		}
+
+		result = append(result, denoms)
+	}
+
+	return
+}
+
 func doEvery(d time.Duration, f func()) {
 	for range time.Tick(d) {
 		go f()
 	}
 }
 
-func createSpammers(cdc *wire.Codec, spamPrefix string, spamPassword string, stats *stats.Stats, chainID string,
-) ([]Spammer, error) {
+func createSpammers(spamCtx *spamCtx, cdc *wire.Codec, spamPrefix string, spamPassword string) ([]Spammer, error) {
 	kb, err := keys.GetKeyBase()
 	if err != nil {
 		return nil, err
@@ -112,7 +147,7 @@ func createSpammers(cdc *wire.Codec, spamPrefix string, spamPassword string, sta
 			j++
 			wg.Add(1)
 			semaphore <- true
-			go SpawnSpammer(cdc, spamPassword, j, kb, info, stats, chainID, &spammers, &wg, semaphore)
+			go SpawnSpammer(spamCtx, cdc, spamPassword, j, kb, info, &spammers, &wg, semaphore)
 		}
 	}
 
@@ -122,8 +157,8 @@ func createSpammers(cdc *wire.Codec, spamPrefix string, spamPassword string, sta
 }
 
 //Spawn new spammer
-func SpawnSpammer(cdc *wire.Codec, spamPassword string, index int, kb cryptokeys.Keybase,
-	info cryptokeys.Info, stats *stats.Stats, chainID string, spammers *[]Spammer, wg *sync.WaitGroup,
+func SpawnSpammer(spamCtx *spamCtx, cdc *wire.Codec, spamPassword string, index int, kb cryptokeys.Keybase,
+	info cryptokeys.Info, spammers *[]Spammer, wg *sync.WaitGroup,
 	semaphore <-chan bool) {
 	defer wg.Done()
 
@@ -139,7 +174,7 @@ func SpawnSpammer(cdc *wire.Codec, spamPassword string, index int, kb cryptokeys
 	txCtx := authctx.TxContext{
 		Codec:   cdc,
 		Gas:     10000,
-		ChainID: chainID,
+		ChainID: spamCtx.chainID,
 	}
 
 	log.Log.Debugf("Spammer %v: Finding account...\n", index)
@@ -165,7 +200,9 @@ func SpawnSpammer(cdc *wire.Codec, spamPassword string, index int, kb cryptokeys
 	queryFree <- true
 
 	newSpammer := Spammer{
-		info.GetName(), spamPassword, address, cdc, index, account.GetSequence(), cliCtx, txCtx, priv, account.GetCoins(), 0, queryFree}
+		info.GetName(), spamPassword, address, cdc, index, account.GetSequence(), cliCtx, txCtx, priv,
+		account.GetCoins(), 0, queryFree,
+	}
 
 	*spammers = append(*spammers, newSpammer)
 	log.Log.Infof("Spammer %v: Spawned...\n", index)
@@ -188,45 +225,29 @@ type Spammer struct {
 	queryFree      chan bool
 }
 
-func (sp *Spammer) send(nextSpammer *Spammer, stats *stats.Stats) {
+func (sp *Spammer) send(spamCtx *spamCtx, nextSpammer *Spammer) {
 	<-sp.queryFree
 
 	log.Log.Debugf("Spammer %v: Will transaction with sequence %v...\n", sp.index, sp.nextSequence)
 
-	// calculate random share of coins to be sent
-	randomCoins := getRandomCoinsUpTo(sp.currentCoins, 100000)
-	clpFrom := "RUNE"
-	clpTo := "XMR"
-	var clpAmount sdk.Int
-
-	clpMsg := rand.Float32() < 0.5
-
+	randType := rand.Float32()
 	var msg sdk.Msg
-	if clpMsg {
-		if rand.Float32() < 0.5 {
-			clpFrom = "XMR"
-			clpTo = "RUNE"
-		}
-		clpAmount = randomCoins.AmountOf(clpFrom)
-		if !clpAmount.GT(sdk.NewInt(0)) {
-			clpFrom = "RUNE"
-			clpTo = "XMR"
-			clpAmount = randomCoins.AmountOf(clpFrom)
-		}
-		if clpAmount.GT(sdk.NewInt(0)) {
-			msg = clpTypes.NewMsgTrade(sp.accountAddress, clpFrom, clpTo, int(clpAmount.Int64()))
+	var coinsUsed sdk.Coins
+	var ok bool
 
-			log.Log.Debugf("Spammer %v: Will trade on CLP: %v %v -> %v\n", sp.index, clpAmount, clpFrom, clpTo)
-		} else {
-			log.Log.Debugf("Spammer %v: Will not trade on CLP, has %v %v and %v %v\n", sp.index, clpAmount, clpFrom, randomCoins.AmountOf(clpTo), clpTo)
-			sp.updateSequenceAndCoins()
-			sp.queryFree <- true
-			return
-		}
-	} else {
-		msg = client.BuildMsg(sp.accountAddress, nextSpammer.accountAddress, randomCoins)
+	switch {
+	case randType < 0.5:
+		msg, coinsUsed, ok = sp.makeRandomTxMsg(nextSpammer)
+	case randType < 0.99:
+		msg, coinsUsed, ok = sp.makeRandomClpMsg()
+	default:
+		msg, coinsUsed, ok = sp.makeRandomLimitOrderMsg(spamCtx)
+	}
 
-		log.Log.Debugf("Spammer %v: Will send: %v to %v\n", sp.index, randomCoins.String(), nextSpammer.accountAddress.String())
+	if !ok {
+		sp.updateSequenceAndCoins()
+		sp.queryFree <- true
+		return
 	}
 
 	sp.txCtx = sp.txCtx.WithSequence(sp.nextSequence)
@@ -238,20 +259,15 @@ func (sp *Spammer) send(nextSpammer *Spammer, stats *stats.Stats) {
 
 	if err != nil {
 		log.Log.Warningf("Spammer %v: Received error trying to send: %v\n", sp.index, err)
-		stats.AddError()
+		spamCtx.stats.AddError()
 		sp.updateSequenceAndCoins()
 		sp.queryFree <- true
 		return
 	}
 	log.Log.Debugf("Spammer %v: Sending successful\n", sp.index)
-	stats.AddSuccess()
+	spamCtx.stats.AddSuccess()
 
-	if clpMsg {
-		sp.currentCoins = sp.currentCoins.Minus([]sdk.Coin{sdk.NewInt64Coin(clpFrom, clpAmount.Int64())})
-	} else {
-		sp.currentCoins = sp.currentCoins.Minus(randomCoins)
-	}
-
+	sp.currentCoins = sp.currentCoins.Minus(coinsUsed)
 	if sp.sequenceCheck >= 200 {
 		sp.updateSequenceAndCoins()
 	}
@@ -303,4 +319,107 @@ func getRandomCoinsUpTo(coins sdk.Coins, divideBy int64) sdk.Coins {
 	}
 
 	return res
+}
+
+func (sp *Spammer) makeRandomClpMsg() (sdk.Msg, sdk.Coins, bool) {
+	clpFrom := "RUNE"
+	clpTo := "XMR"
+
+	if rand.Float32() < 0.5 {
+		clpFrom = "XMR"
+		clpTo = "RUNE"
+	}
+
+	randomCoins := getRandomCoinsUpTo(sp.currentCoins, 100000)
+	clpAmount := randomCoins.AmountOf(clpFrom)
+
+	if !clpAmount.GT(sdk.NewInt(0)) {
+		clpFrom = "RUNE"
+		clpTo = "XMR"
+		clpAmount = randomCoins.AmountOf(clpFrom)
+	}
+
+	if !clpAmount.GT(sdk.NewInt(0)) {
+		log.Log.Debugf("Spammer %v: Will not trade on CLP, has %v %v and %v %v\n", sp.index, clpAmount, clpFrom, randomCoins.AmountOf(clpTo), clpTo)
+		return nil, sdk.Coins{}, false
+	}
+
+	msg := clpTypes.NewMsgTrade(sp.accountAddress, clpFrom, clpTo, int(clpAmount.Int64()))
+
+	log.Log.Debugf("Spammer %v: Will trade on CLP: %v %v -> %v\n", sp.index, clpAmount, clpFrom, clpTo)
+
+	return msg, sdk.Coins{sdk.NewCoin(clpFrom, clpAmount)}, true
+}
+
+func (sp *Spammer) makeRandomTxMsg(nextSpammer *Spammer) (sdk.Msg, sdk.Coins, bool) {
+	randomCoins := getRandomCoinsUpTo(sp.currentCoins, 100000)
+	msg := client.BuildMsg(sp.accountAddress, nextSpammer.accountAddress, randomCoins)
+
+	log.Log.Debugf("Spammer %v: Will send: %v to %v\n", sp.index, randomCoins.String(), nextSpammer.accountAddress.String())
+
+	return msg, randomCoins, true
+}
+
+func (sp *Spammer) makeRandomLimitOrderMsg(spamCtx *spamCtx) (sdk.Msg, sdk.Coins, bool) {
+	amtDenom, priceDenom := getRandomLimitOrderDenoms(spamCtx)
+
+	randomCoins := getRandomCoinsUpTo(sp.currentCoins, 1000)
+
+	amtIfSell := sdk.NewCoin(amtDenom, randomCoins.AmountOf(amtDenom))
+	totalPriceIfBuy := sdk.NewCoin(priceDenom, randomCoins.AmountOf(priceDenom))
+
+	buy := true
+
+	if rand.Float32() < 0.5 {
+		buy = false
+	}
+
+	// check if amt/totalPrice > 0
+	possibleKinds := 2
+	if !totalPriceIfBuy.Amount.GT(sdk.NewInt(0)) {
+		buy = false
+		possibleKinds--
+	}
+	if !amtIfSell.Amount.GT(sdk.NewInt(0)) {
+		buy = true
+		possibleKinds--
+	}
+	if possibleKinds == 0 {
+		log.Log.Debugf("Spammer %v: Will not create limit order, amtIfSell %v and totalPriceIfBuy %v\n", sp.index,
+			amtIfSell, totalPriceIfBuy)
+		return nil, sdk.Coins{}, false
+	}
+
+	var kind exchange.OrderKind
+	var amt sdk.Coin
+	var price sdk.Coin
+	var coinsUsed sdk.Coin
+
+	if buy {
+		kind = exchange.BuyOrder
+		price = sdk.NewInt64Coin(priceDenom, rand.Int63n(totalPriceIfBuy.Amount.Int64())+1) // TODO use market prices
+		amt = sdk.NewCoin(amtDenom, totalPriceIfBuy.Amount.Div(price.Amount))
+		coinsUsed = totalPriceIfBuy
+	} else {
+		kind = exchange.SellOrder
+		price = sdk.NewInt64Coin(priceDenom, rand.Int63n(amtIfSell.Amount.Int64())+1)
+		amt = amtIfSell
+		coinsUsed = amtIfSell
+	}
+
+	msg := exchange.NewMsgCreateLimitOrder(sp.accountAddress, kind, amt, price, time.Now().Add(24*time.Hour))
+
+	log.Log.Debugf("Spammer %v: Will create limit order, buy? %v with amt %v and price %v\\n", sp.index, buy, amt,
+		price)
+
+	return msg, sdk.Coins{coinsUsed}, true
+}
+
+func getRandomLimitOrderDenoms(spamCtx *spamCtx) (string, string) {
+	randomPair := spamCtx.limitOrderPairs[rand.Intn(len(spamCtx.limitOrderPairs))]
+	return randomPair[0], randomPair[1]
+}
+
+func getTotalPrice(amt sdk.Coin, price sdk.Coin) sdk.Coin {
+	return sdk.Coin{price.Denom, amt.Amount.Mul(price.Amount)}
 }
